438af7739a0106208dd25ae9c2ccc69649cd3fd2
Christopher <bopherchan@gmail.com>
2023-02-06 10:42:12 -0500

    practice tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let newTaskDescription = document.getElementById("new-task-description");
+  let form = document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(newTaskDescription.value)
+     buildToDo(newTaskDescription.value)
+     e.target.reset()
+  })
+ 
 });
+
+function buildToDo(todo){
+  let p = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+
+}
